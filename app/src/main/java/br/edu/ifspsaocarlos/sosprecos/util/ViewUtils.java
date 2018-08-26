e9daec3b23bda2bd60d309dfdc475ac6d4b2b741
Andrey Brugnera <andreybrugnera@gmail.com>
2018-08-26 12:21:22 -0300

    CHanged progressBar color and removed unused classes.

--- app/src/main/java/br/edu/ifspsaocarlos/sosprecos/util/ViewUtils.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sosprecos/util/ViewUtils.java
@@ -1,8 +1,11 @@
 package br.edu.ifspsaocarlos.sosprecos.util;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 /**
  * Created by Andrey R. Brugnera on 22/08/2018.
@@ -10,6 +13,12 @@ import android.widget.FrameLayout;
 public class ViewUtils {
 
     public static void showProgressBar(FrameLayout progressBarHolder) {
+        ProgressBar progressBar = (ProgressBar) progressBarHolder.getChildAt(0);
+
+        Drawable progressDrawable = progressBar.getIndeterminateDrawable().mutate();
+        progressDrawable.setColorFilter(Color.BLACK, android.graphics.PorterDuff.Mode.SRC_IN);
+        progressBar.setProgressDrawable(progressDrawable);
+
         AlphaAnimation inAnimation = new AlphaAnimation(0f, 1f);
         inAnimation.setDuration(200);
         progressBarHolder.setAnimation(inAnimation);
